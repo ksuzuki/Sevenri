5546077b2b15514c04e22e4faa6bb85030241a97
Kei Suzuki <ksdevx@gmail.com>
2011-03-13 12:06:07 +0900

    planter: changes to get lein47ri-1.5.0-RC1 working

--- src/project/slix/planter/project.clj
+++ src/project/slix/planter/project.clj
@@ -11,7 +11,6 @@
 
 (defproject slix.planter "0.1.0"
   :description "slix.planter project: stub to get lein deps for lein47ri"
-  :dependencies [[org.apache.ant/ant "1.7.1"]
-                 [org.apache.ant/ant-nodeps "1.7.1"]
+  :dependencies [[lancet "1.0.0"]
                  [robert/hooke "1.1.0"]
                  [org.apache.maven/maven-ant-tasks "2.0.10" :exclusions [ant]]])
