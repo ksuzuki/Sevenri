2f0a4baa33b04bb48ca8534a1cd35b92fabe4293
Kei Suzuki <ksdevx@gmail.com>
2011-04-23 13:16:33 +0900

    repl: improve the ls cmd defined in replrc

--- src/slix/repl/replrc.clj
+++ src/slix/repl/replrc.clj
@@ -114,10 +114,71 @@
   [file]
   `(sh "cat" ~file))
 
-(defn ls
-  [root]
-  (let [rep (re-pattern (str root "/[^/]*"))]
-    (ps (find-files #(re-matches rep (str %)) root))))
+(defmacro ls
+  "Emulate ls command. Default path is 'user.dir'.
+   ls options should begin with '-' but no need to quote.
+   File args can be either string, File object, or quoted symbol.
+   Depends on REPL's property :console."
+  [& args]
+  ;; Convert the ls options arg into string if any.
+  (let [args (if (= (first (str (first args))) \-)
+               (apply list (str (first args)) (rest args))
+               args)]
+    `(let [argvals# [~@args]
+           options# (when (= (first (str (first argvals#))) \-) (first argvals#))
+           longout# (and options# (re-find #"l" options#)) ;; detect long output
+           pathlst# (or (seq (if options# (rest argvals#) argvals#))
+                        (list (get-user-path)))]
+       #_(println "argvals:" argvals# "options:" options# "longout:" longout# "pathlst:" pathlst#)
+       (doseq [path# pathlst#]
+         (let [abpath# (if (instance? java.io.File path#)
+                         path#
+                         (java.io.File. (sym2path (obj2sym path#))))
+               pathnm# (if (.isAbsolute abpath#)
+                         (str abpath#)
+                         (str (java.io.File. (get-user-path) (str abpath#))))
+               result# (apply cjsh "ls" (filter identity [options# pathnm# :out-enc "UTF-8"]))]
+           ;; Print target path and the sh result.
+           (println (str path# ":"))
+           (if (and (zero? (:exit result#)) (empty? (:err result#)))
+             (let [items# (seq (.split (:out result#) "\n"))]
+               (if longout#
+                 ;; Print items per-line
+                 (do
+                   (apply println (interpose "\n" items#))
+                   (println))
+                 ;; Print items in multiple columns, like 'ls' does.
+                 (let [itc# (count items#)
+                       ;; Find longest item and its length.
+                       mxa# (reduce (fn [a# s#]
+                                      (let [cs# (count s#)]
+                                        (if (< (second a#) cs#)
+                                          [s# cs#]
+                                          a#)))
+                                    ["" 0]
+                                    items#)
+                       ;; Dig out FontMetrics.
+                       txp# (.getTextPane (get-slix-prop :console))
+                       fmx# (.getFontMetrics txp# (.getFont txp#))
+                       ;; Figure out required columns and rows. OK to use
+                       ;; the width of the textpane because it's mapped to
+                       ;; its viewport 1:1.
+                       mxw# (.stringWidth fmx# (str (first mxa#) "  "))
+                       clm# (int (Math/floor (/ (.getWidth txp#) mxw#)))
+                       row# (int (Math/ceil (/ itc# clm#)))
+                       ;; Format each item in left-justified and space padded
+                       ;; fixed size, preceeded by \newline optionally.
+                       fmt# (str "%s%-" (inc (second mxa#)) "s")
+                       its# (for [r# (range row#) c# (range clm#)]
+                              (let [brk# (if (and (pos? r#) (zero? c#)) "\n" "")
+                                    idx# (+ r# (* c# row#))
+                                    itm# (if (< idx# itc#) (nth items# idx#) "")]
+                                (format fmt# brk# itm#)))]
+                   #_(println "mxa:" mxa# "clm:" clm# "row:" row#)
+                   (apply print its#)
+                   (println "\n"))))
+             ;; 'sh ls' failed.
+             (println "ERR:" (:err result#))))))))
 
 (defn open-sevenri-users-manual
   []
